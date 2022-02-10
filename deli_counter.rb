cd9550ec3c521526543d47d6c86f818bba84e601
Catherine Loesch <catherine.loesch@protonmail.com>
2022-02-10 01:12:01 +0000

    refactoring

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,21 @@
-katz_deli = []
-
 def line(arr)
-    if arr.size == 0
-       puts "The line is currently empty."
-    else 
-       customers_with_number = arr.map.with_index do |customer, index|
-        (index+1).to_s + ". " + customer
-       end
-       puts "The line is currently: #{customers_with_number.join(" ")}"
+    if arr.empty?
+        puts "The line is currently empty."
+    else
+        list = arr.map.with_index(1) {|name, index| "#{index}. #{name}"}.join(" ")
+        puts "The line is currently: " + list
     end
 end
 
-def take_a_number(arr, name)
-    arr << name
-    puts "Welcome, #{name}. You are number #{arr.size} in line."
+def take_a_number(arr, new_person)
+    arr << new_person
+    puts "Welcome, #{new_person}. You are number #{arr.length} in line."
 end
 
 def now_serving(arr)
-    if arr.size == 0
+    if arr.empty?
         puts "There is nobody waiting to be served!"
-    else 
-        puts "Currently serving #{arr[0]}."
-        arr.shift
+    else
+        puts "Currently serving #{arr.shift}."
     end
 end
-
